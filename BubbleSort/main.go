9b08a714d143240c3f0af7439ed044f024b6e338
Alan.Lin <eric820209@gmail.com>
2020-12-08 22:33:14 +0800

    first commit

---
+++ BubbleSort/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nums = []int{1, 5, 7, 89, 3, 7, 45}
+	var length = len(nums)
+
+	for t := 0; t < length; t++ {
+		for i := 0; i < length-t-1; i++ {
+			if nums[i] > nums[i+1] {
+				var temp = nums[i]
+				nums[i] = nums[i+1]
+				nums[i+1] = temp
+			}
+		}
+	}
+	fmt.Println(nums)
+}
