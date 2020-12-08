878f75a2edeccf5a23e7e5d40070e6a9e9908522
Alan.Lin <eric820209@gmail.com>
2020-12-09 00:14:39 +0800

    finish test

--- Sort/bubble/bubblesort_test.go
+++ Sort/bubble/bubblesort_test.go
@@ -1,8 +1,16 @@
 package bubble
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestBubbleSort(t *testing.T) {
-	var nums1 = []int{1, 2, 3, 5, 8}
-	Sort(&nums1)
+	var nums = []int{1, 5, 8, 2, 3}
+	Sort(&nums)
+	var exp = []int{1, 2, 3, 5, 8}
+	var same = reflect.DeepEqual(nums, exp)
+	if !same {
+		t.Error("Not same")
+	}
 }
