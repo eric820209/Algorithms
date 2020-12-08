9b096fdf6d3716de1f9a7245976a152e067b5543
Alan.Lin <eric820209@gmail.com>
2020-12-09 01:45:52 +0800

    Misunderstanding 選擇排序只交換最小與第一個數

--- mySort/selection/selectionsort.go
+++ mySort/selection/selectionsort.go
@@ -7,15 +7,13 @@ func Sort(slc *[]int) {
 	var m int
 	for i := 0; i < length-1; i++ {
 		m = i
+		var temp = nums[i]
 		for s := i + 1; s < length; s++ {
 			if nums[m] > nums[s] {
 				m = s
 			}
 		}
-		var temp = nums[m]
-		for j := m; j > i; j-- {
-			nums[j] = nums[j-1]
-		}
-		nums[i] = temp
+		nums[i] = nums[m]
+		nums[m] = temp
 	}
 }
